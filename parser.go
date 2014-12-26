700e74ebdded6ffc861d213f8abdb3b132d8f01f
Tom Milewski <tmilewski@gmail.com>
2014-12-26 05:52:31 -0500

    Convert conditional to guard clause

--- parser.go
+++ parser.go
@@ -17,45 +17,47 @@ func parse(input string) (map[string]string, error) {
 	for _, line := range lines {
 		line = strings.TrimSpace(line)
 
-		if len(line) != 0 {
-			match, _ := regexp.MatchString(regex, line)
-			matchFormat, _ := regexp.MatchString(formatRegex, line)
+		if len(line) == 0 {
+			continue
+		}
 
-			if match {
-				res := re.FindAllStringSubmatch(line, -1)
+		match, _ := regexp.MatchString(regex, line)
+		matchFormat, _ := regexp.MatchString(formatRegex, line)
 
-				if res != nil {
-					key, value := res[0][1], res[0][2]
+		if match {
+			res := re.FindAllStringSubmatch(line, -1)
 
-					// Remove surrounding quotes
-					quotes := regexp.MustCompile(quotesRegex)
-					q := quotes.FindAllStringSubmatch(value, -1)
+			if res != nil {
+				key, value := res[0][1], res[0][2]
 
-					quoteType := ""
+				// Remove surrounding quotes
+				quotes := regexp.MustCompile(quotesRegex)
+				q := quotes.FindAllStringSubmatch(value, -1)
 
-					if len(q) > 0 {
-						quoteType = q[0][1]
-						value = q[0][2]
-					}
+				quoteType := ""
 
-					if quoteType == `"` {
-						r := regexp.MustCompile(`\\n`)
-						value = r.ReplaceAllString(value, "\n")
-
-						r = regexp.MustCompile(`\\([^$])`)
-						value = r.ReplaceAllString(value, "${1}")
-					}
-
-					if quoteType != `'` {
-						value = substituteVariables(value, envs)
-						// TODO: Substitute Commands
-					}
-
-					envs[key] = strings.TrimSpace(value)
+				if len(q) > 0 {
+					quoteType = q[0][1]
+					value = q[0][2]
 				}
-			} else if !matchFormat {
-				return map[string]string{}, fmt.Errorf("FormatError: Line `%s` doesn't match format", line)
+
+				if quoteType == `"` {
+					r := regexp.MustCompile(`\\n`)
+					value = r.ReplaceAllString(value, "\n")
+
+					r = regexp.MustCompile(`\\([^$])`)
+					value = r.ReplaceAllString(value, "${1}")
+				}
+
+				if quoteType != `'` {
+					value = substituteVariables(value, envs)
+					// TODO: Substitute Commands
+				}
+
+				envs[key] = strings.TrimSpace(value)
 			}
+		} else if !matchFormat {
+			return map[string]string{}, fmt.Errorf("FormatError: Line `%s` doesn't match format", line)
 		}
 	}
 
